c5231b9a3b4b9f022268373ad8230c91a1dfa6d1
datalinx <nzoogii@gmail.com>
2026-09-01 12:39:37 +0800

    Route wholesale page to online store

--- app/[[...slug]]/page.tsx
+++ app/[[...slug]]/page.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-sync-scripts */
 import AdminClient from "../admin/AdminClient";
 import RebaClient from "../reba/RebaClient";
+import WholesaleStoreClient from "../store/WholesaleStoreClient";
 import UguumjArkhadPrototype from "../UguumjArkhadPrototype";
 
 export default async function SiteRoute({ params }: { params: Promise<{ slug?: string[] }> }) {
@@ -8,6 +9,7 @@ export default async function SiteRoute({ params }: { params: Promise<{ slug?: s
   if (route.slug?.[0] === "admin") return <AdminClient />;
   if (route.slug?.join("/") === "reba-vintage-cafe") return <RebaClient />;
   if (route.slug?.join("/") === "shop") return <UguumjArkhadPrototype />;
+  if (route.slug?.join("/") === "wholesale") return <WholesaleStoreClient />;
 
   return (
     <>
